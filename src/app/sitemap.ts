7794937dcf1afbac13147f987da1e6a91c269477
Przemyslaw Chojecki <p@monkin.co>
2024-09-01 16:50:56 +0200

    Test sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,22 @@
+import { promises as fs } from "fs";
+import type { MetadataRoute } from "next";
+import path from "path";
+
+async function getAppRoutes(): Promise<string[]> {
+  const appDir = path.join(process.cwd(), "src/app");
+  const files = await fs.readdir(appDir, { withFileTypes: true });
+
+  return files.filter((file) => file.isDirectory()).map((file) => file.name);
+}
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const routes = await getAppRoutes();
+  const baseUrl = "https://monkin.co";
+
+  return routes.map((route) => ({
+    url: `${baseUrl}/${route}`,
+    lastModified: new Date().toISOString(),
+    changeFrequency: "weekly",
+    priority: 1,
+  }));
+}
