488422edfead63daf07f00a3c1467c3c0126e377
Przemyslaw Chojecki <p@monkin.co>
2024-09-14 17:07:09 +0200

    haha

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -1,4 +1,4 @@
 export const globalDescription = {
   description:
-    "Creating websites is easy. Building products that form a relationship with your users is not. That's where I come in.",
+    "haha Creating websites is easy. Building products that form a relationship with your users is not. That's where I come in.",
 };
